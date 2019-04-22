3e3d90944f6ee077d5e7a399f7680d021f2d195c
v.pylypenko <v.pylypenko@pointerbp.com>
2019-04-22 18:26:46 +0300

    1

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -61,23 +61,6 @@
                 </li>
             </ul> <!-- end header__social -->
 
-            <a class="header__search-trigger" href="#0"></a>
-
-            <div class="header__search">
-
-                <form role="search" method="get" class="header__search-form" action="#">
-                    <label>
-                        <span class="hide-content">Search for:</span>
-                        <input type="search" class="search-field" placeholder="Type Keywords" value="" name="s" title="Search for:" autocomplete="off">
-                    </label>
-                    <input type="submit" class="search-submit" value="Search">
-                </form>
-
-                <a href="#0" title="Close Search" class="header__overlay-close">Close</a>
-
-            </div>  <!-- end header__search -->
-
-
             <a class="header__toggle-menu" href="#0" title="Menu"><span>Menu</span></a>
 
             <nav class="header__nav-wrap">
